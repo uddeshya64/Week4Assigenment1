78c3bcfd65e216aa819b418cfe4741a03dac26bd
Uddeshya Patidar <113989316+uddeshya64@users.noreply.github.com>
2024-09-26 16:19:33 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,23 @@
+/***
+ * Task: Hardcoded values for Java program to convert and perform mathematical operation on various number system.
+ * @author Uddeshya Patidar
+ * Date: 26/09/2024
+ */
+package practice.java;
+
+//Constant class to manage hardcoded values
+public class Constants {
+	public static final String Welcome = "What would you like to do?\n1. Convert a number between systems\n2. Perform an arithmetic operation (Addition/Subtraction)";
+	public static final String ConversionIndex = "Enter the type of input number:\n1. Decimal\n2. Binary\n3. Octal\n4. Hexadecimal";
+	public static final String InvalidConversionIndex = "Invalid input type. Please enter a number between 1 and 4.";
+	public static final String UserInput = "Enter the number to convert: ";
+	public static final String ValibInputCheck = "Invalid input. Please enter a valid number without spaces.";
+	public static final String NegtiveInput = "Invalid input. Try again (only positive integers).";
+	public static final String ReturnTypeIndex = "Convert the number to:\n1. Decimal\n2. Binary\n3. Octal\n4. Hexadecimal"; 
+	public static final String ArthemticOperationIndex = "Convert the number to:\n1. Decimal\n2. Binary\n3. Octal\n4. Hexadecimal"; 
+	public static final String EnterNumber = "Enter a number (or type 'done' to finish): ";
+	public static final String InvalidInput = "Invalid input. Please enter a valid number without spaces and positive integer.";
+	public static final String ArthemticOptions = "What operation would you like to perform:\n1. Addition\n2. Subtraction";
+	public static final String ArthemticInvalidOption = "Invalid operation. Please select 1 or 2.";
+	public static final String RunAgain = "Would you like to run the program again? (y/n)";
+}
